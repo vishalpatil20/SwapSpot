040c101e46ca759e856fdfb70ec32fc08cf47032
Vishal Patil <rvish9999@gmail.com>
2024-04-14 22:55:32 +0530

    navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const navigation = [
   { name: 'Home', href: '/', current: true },
   { name: 'About', href: '/Aboutme', current: false },
   { name: 'Services', href: '/services', current: false },
-  { name: 'Login/Sign-up', href: '/Login', current: false },
   { name: 'Play', href: '/PLAY', current: false },
+  { name: 'Login/Sign-up', href: '/Login', current: false },
 ];
 
 function classNames(...classes) {
@@ -90,12 +90,12 @@ export default function Example() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md text-[#004AAD] py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md text-[#004AAD] py-1 shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white')}
+                            className={classNames(active ? 'bg-[#004AAD]' : '', 'block px-4 py-2 text-sm text-white')}
                           >
                             Your Profile
                           </a>
@@ -105,7 +105,7 @@ export default function Example() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white')}
+                            className={classNames(active ? 'bg-[#004AAD]' : '', 'block px-4 py-2 text-sm text-white')}
                           >
                             Settings
                           </a>
@@ -115,7 +115,7 @@ export default function Example() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white')}
+                            className={classNames(active ? 'bg-red-500' : '', 'block px-4 py-2 text-sm text-white')}
                           >
                             Sign out
                           </a>
