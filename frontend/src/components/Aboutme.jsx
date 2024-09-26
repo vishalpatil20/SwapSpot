a014304d2b78c5e91897ac1fbb2700b8fa8614f9
vishalpatil20 <rvish9999@gmail.com>
2024-09-26 14:12:09 +0530

    Config updates

--- frontend/src/components/Aboutme.jsx
+++ frontend/src/components/Aboutme.jsx
@@ -5,7 +5,7 @@ import Swap from './SwipeFeature/Swap'
 const Aboutme = () => {
   return (
     <div>
-      <div className="relative overflow-hidden bg-black">
+      <div className="relative h-screen overflow-hidden bg-black">
       <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
